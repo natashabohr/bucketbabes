f5e25e98ad1e2f8910092f8ed7da15cdff6a3ab4
Ka Kay Pang <kakay.pang@hotmail.co.uk>
2018-12-05 19:50:02 +0000

    inital framework by kakay

---
+++ comp/bucketbabes.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask("MyApp")
+# can add additional page after /
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+app.run(debug=True)
