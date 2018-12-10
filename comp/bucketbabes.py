e458fdbcf6970bed799c77258933468033d23ab3
Ka Kay Pang <kakay.pang@hotmail.co.uk>
2018-12-10 17:27:05 +0000

    Fixed background, Jinja base template, and complete MailGun API

--- comp/bucketbabes.py
+++ comp/bucketbabes.py
@@ -17,4 +17,35 @@ def contact():
 def about():
     return render_template("about.html")
 
+@app.route("/sent", methods=["POST"])
+def sent():
+    return render_template("sent.html")
+
+@app.route("/contact", methods=["POST"])
+def contact_form():
+    form_data = request.form
+    send_simple_message(form_data["email"], form_data["name"])
+    receive_message(form_data["email"], form_data["name"], form_data["message"])
+    return render_template("sent.html")
+
+def send_simple_message(nosy_person, name):
+    form_data = request.form
+    return requests.post(
+        "https://api.mailgun.net/v3/sandbox0e4f6c1364cd4dd4b37fea67b9afc7c6.mailgun.org/messages",
+        auth=("api", "c9b7fafb60c13e46ba3e46ea3a6d94dd-059e099e-c9e3fdee"),
+        data={"from": "STUCK <mailgun@sandbox0e4f6c1364cd4dd4b37fea67b9afc7c6.mailgun.org>",
+              "to": nosy_person,
+              "subject": "We've recieved your message!",
+              "html": render_template("autoresponse.html", names=name.title())})
+
+def receive_message(email, name, message):
+    form_data = request.form
+    return requests.post(
+        "https://api.mailgun.net/v3/sandbox0e4f6c1364cd4dd4b37fea67b9afc7c6.mailgun.org/messages",
+        auth=("api", "c9b7fafb60c13e46ba3e46ea3a6d94dd-059e099e-c9e3fdee"),
+        data={"from": "STUCK Message <mailgun@sandbox0e4f6c1364cd4dd4b37fea67b9afc7c6.mailgun.org>",
+              "to": "kakay.pang@hotmail.co.uk",
+              "subject": "STUCK Message from {}".format(name.title()),
+              "html": render_template("message.html", names=name.title(), emails=email.lower(), messages=message)})
+
 app.run(debug=True)
